f54c5f124a2b61d4235ea6fdbfaa0ce280146cc2
keithytsai(蔡和洋) <keithytsai@tencent.com>
2016-04-06 11:10:21 +0800

    Remove comment code fragment

--- src/js/components/com/Header.js
+++ src/js/components/com/Header.js
@@ -2,15 +2,6 @@ import React from 'react';
 
 import 'css/com/_header';
 
-                // <span class="arrow"></span>
-                // <div class="sub">
-                //   <ul>
-                //     <li><a href="">移动应用接入</a></li>
-                //     <li><a href="">网页应用接入</a></li>
-                //     <li><a href="">智能硬件</a></li>
-                //     <li><a href="">资料库</a></li>
-                //   </ul>
-                // </div>
 class Header extends React.Component {
   render() {
     return (
