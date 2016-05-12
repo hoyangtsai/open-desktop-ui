ce30a6da4b94885827966faaf823b0d5c25d1cda
keithytsai <keithytsai@tencent.com>
2016-05-12 21:45:29 +0800

    Add normalize style

--- src/js/components/Button.js
+++ src/js/components/Button.js
@@ -1,33 +1,40 @@
 import React from 'react';
-
 import AsideNav from './com/AsideNav';
+
 import 'css/com/_aside-nav';
 import 'css/com/_button';
 
 import ButtonSpec from 'img/design-spec/button.png';
+
+var btnDemo = {
+  marginRight: '8px'
+};
+
+        // <div className="btn-spec-wrap">
+        //   <img src={ButtonSpec} alt="button spec" />
+        // </div>
+        //
+
+
+var btnCode = '<a href="" className="ui-button">正常按钮</a>' +
+    '<a href="" className="ui-button ui-button-primary">突出</a>' +
+    '<a href="" className="ui-button ui-button-suc">重点突出</a>';
 
 class Button extends React.Component {
   render() {
     return (
       <div className="layout">
         <AsideNav />
-        <div className="btn-spec-wrap">
-          <img src={ButtonSpec} alt="button spec" />
-        </div>
+        <h3>代码演示</h3>
         <div>
-            <a href="" className="ui-button">正常按钮</a>
-            <a href="" className="ui-button ui-button-primary">突出</a>
-            <a href="" className="ui-button ui-button-suc">重点突出</a>
-            <a href="" className="ui-button">很多文字时候的样子</a>
-            <a href="" className="ui-button disabled">不可用状态</a>
-            <a href="" className="ui-button ui-button-small">小按钮</a>
-            <a href="" className="ui-button ui-button-small ui-button-primary">小按钮</a>
-            <a href="" className="ui-button ui-button-small ui-button-suc">小按钮</a>
-            <div className="ui-button ui-button-primary mt10">整块都是按钮</div>
-        </div>
-        <div>
-          <h2>无样式按钮</h2>
-          <a href="" className="ui-button">无样式</a>
+            <a href="" className="ui-button" style={btnDemo}>正常按钮</a>
+            <a href="" className="ui-button ui-button-primary" style={btnDemo}>幽灵按钮</a>
+            <a href="" className="ui-button ui-button-suc" style={btnDemo}>强调按钮</a>
+            <pre>
+            <code className="language-javascript">
+              {btnCode}
+            </code>
+            </pre>
         </div>
         <div>
           <h2>一般按钮</h2>
